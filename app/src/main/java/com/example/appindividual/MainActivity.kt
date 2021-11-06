b684264084b52e1986b657d45bd8b790af80d25a
nemequene <93455552+nemequene@users.noreply.github.com>
2021-11-05 22:50:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appindividual/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appindividual
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var sNombres: String = ""
+    private var sApellidos: String = ""
+    private var sTelefono: String = ""
+    private var sEmail: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+        //btnSuscribir.setOnClickListener{}
+
+}
